78848499fb2f364a8b16fdc6274cecdd974191c1
53214mm <153275736+53214mm@users.noreply.github.com>
2026-03-31 11:28:51 +0800

    初始项目

---
+++ src/main/java/com/li/liaiagent/controller/HiController.java
@@ -0,0 +1,15 @@
+package com.li.liaiagent.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hi")
+public class HiController {
+    @GetMapping
+    public String Hi() {
+        return "hi";
+    }
+}
---
+++ src/test/java/com/li/liaiagent/LiAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.liaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
