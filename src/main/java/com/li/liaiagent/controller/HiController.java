740503126f599e5f54f159c469fcdede21708281
53214mm <153275736+53214mm@users.noreply.github.com>
2026-03-31 11:30:41 +0800

    初始项目1

--- src/main/java/com/li/liaiagent/controller/HiController.java
+++ src/main/java/com/li/liaiagent/controller/HiController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HiController {
     @GetMapping
     public String Hi() {
-        return "hi";
+        return "Hi";
     }
 }
